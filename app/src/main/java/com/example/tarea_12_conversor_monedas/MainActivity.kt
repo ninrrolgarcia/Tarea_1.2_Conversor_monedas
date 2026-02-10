5f822a66239fcdaac3961e420c42f09c045246ba
ninrrolgarcia <negarcia1@uth.hn>
2026-02-10 15:54:44 -0600

    Initial commit

---
+++ app/src/main/java/com/example/tarea_12_conversor_monedas/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.tarea_12_conversor_monedas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tarea_12_conversor_monedas.ui.theme.Tarea_12_Conversor_monedasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Tarea_12_Conversor_monedasTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Tarea_12_Conversor_monedasTheme {
+        Greeting("Android")
+    }
+}
